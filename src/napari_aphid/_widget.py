eed348c16933594b0d2255a0199fbe92a3a69bc3
hereariim <herearii.metuarea@gmail.com>
2022-09-12 15:48:45 +0200

    dsd

--- src/napari_aphid/_widget.py
+++ src/napari_aphid/_widget.py
@@ -1,6 +1,6 @@
 from tkinter import Image
 from tkinter.ttk import Progressbar
-
+from tifffile import imsave
 import skimage
 from skimage.io import imread, imshow, imread_collection, concatenate_images
 from skimage import img_as_uint, measure
@@ -8,6 +8,8 @@ from skimage.transform import resize
 from skimage.filters import threshold_multiotsu
 from skimage.measure import label, regionprops_table
 from skimage.io import imsave
+from skimage.segmentation import watershed
+from skimage.feature import peak_local_max
 import skimage.io
 
 import os
@@ -52,6 +54,7 @@ import matplotlib.cm as cm
 from fileinput import filename
 from glob import glob
 from scipy import ndimage as ndi
+import matplotlib.pyplot as plt
 
 from qtpy.QtWidgets import QListWidget
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
@@ -60,6 +63,12 @@ from qtpy.QtWidgets import QListWidget
 from qtpy.QtCore import Qt
 
 zip_dir = tempfile.TemporaryDirectory()
+
+
+def PolyArea2D(pts):
+    lines = np.hstack([pts,np.roll(pts,-1,axis=0)])
+    area = 0.5*abs(sum(x1*y2-x2*y1 for x1,y1,x2,y2 in lines))
+    return area
 
 def table_to_widget(table: dict) -> QWidget:
     """
@@ -206,8 +215,22 @@ def get_quantitative_data_all_for_csv(dossier_des_images,napari_viewer):
     dock_widget = table_to_widget(d)
     napari_viewer.window.add_dock_widget(dock_widget, area='right',name="Save")
     
-
-    
+def define_marker(labels):
+    n,m = labels.shape
+    for ij in range(n):
+        lit_temp=labels[ij]
+        i=0
+        while i<(len(lit_temp)-1):
+            x = lit_temp[i]
+            y = lit_temp[i+1]
+            if x!=y:
+                if x!=0:
+                    if y!=0:
+                        lit_temp[i]=0
+                        lit_temp[i+1]=0
+            i+=1
+    return labels
+        
     
 @magic_factory(call_button="Run segmentation",filename={"label": "Pick a file:"})
 def process_function_segmentation(napari_viewer : Viewer,filename=pathlib.Path.cwd()): 
@@ -247,12 +270,12 @@ def process_function_segmentation(napari_viewer : Viewer,filename=pathlib.Path.c
         print(os.path.join(output_dir.name,path.split('/')[-1][:-3])+'_result_type.tif')
         SEG.append(os.path.join(output_dir.name,path.split('/')[-1][:-3])+'_result_type.tif')
     
-    names =  []
+    #names =  []
     for ix in range(len(abs_path_image_tif)):
         ab1 = abs_path_image_tif[ix].split('/')[-2]
         ab2 = abs_path_image_tif[ix].split('/')[-1][:-4].replace('.','_')
         name_folder = ab1+'_'+ab2
-        names.append(name_folder)
+        #names.append(name_folder)
         path_folder = os.path.join(output_dir.name,name_folder)
 
         os.mkdir(path_folder)
@@ -270,11 +293,137 @@ def process_function_segmentation(napari_viewer : Viewer,filename=pathlib.Path.c
         print(">",new_image_mask_tif_math)
 
     ######
+    
+    MASK = []
+    for ix in os.listdir(output_dir.name):
+        path_temp = os.path.join(output_dir.name,ix)
+        for subfolder in os.listdir(path_temp):
+            path_file = os.path.join(path_temp,subfolder)
+            if path_file.endswith('result_type.tif'):
+                MASK.append(path_file)
+                
+    dico_dico = {}
+    for iw in range(len(MASK)):
+        one_image = imread(MASK[iw])
+        
+        data = np.array(one_image)
+        fond_image=np.where(data==85)
+        bord=np.where(data==170)
+        aphid=np.where(data==255)
+        data[fond_image]=0
+        data[bord]=255
+        data[aphid]=255
+
+        data_fill = ndi.binary_fill_holes(data).astype(int)
+
+        data = np.array(data_fill)
+        fond_image=np.where(data==0)
+        aphid=np.where(data==1)
+        data[fond_image]=0
+        data[aphid]=255
+        
+        name_image = MASK[iw].split('\\')[-1].replace('_type','')[:-3]+'png'
+        
+        image_with_two_classes = os.path.join('\\'.join(MASK[iw].split('\\')[:-1]),name_image) #new name for image with two classes
+        dico_dico[image_with_two_classes]=data
+        plt.imsave(image_with_two_classes, data, cmap = plt.cm.gray)
+    
+    L = list(dico_dico.keys())    
+    
+    dico = {}
+    for iw in range(len(L)):
+        img = cv2.imread(L[iw])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        ret,thresh_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+        thresh_img = 255- thresh_img
+        contours, hierarchy = cv2.findContours(thresh_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+        hull = []
+
+        # calculate points for each contour
+        for i in range(len(contours)):
+            # creating convex hull object for each contour
+            hull.append(cv2.convexHull(contours[i], False))
+        # create an empty black image
+        drawing = np.zeros((thresh_img.shape[0], thresh_img.shape[1], 3), np.uint8)
+        dico[L[iw]] = [contours,hull]
+    
+
+       
+    dico_out = {}
+    for iw in dico.keys():
+        L = dico[iw]
+        contours = L[0]
+        hull = L[1]
+
+        area_hull_list = []
+        area_contour_list = []
+        diff = []
+
+        for i in range(len(contours)):
+            new_list_hull = []
+            for ix in range(len(hull[i])):
+                new_list_hull.append(list(hull[i][ix][0]))
+
+            new_list_contours = []
+            for ix in range(len(contours[i])):
+                new_list_contours.append(list(contours[i][ix][0]))
+
+            area_of_hull = PolyArea2D(new_list_hull)
+            area_of_contour = PolyArea2D(new_list_contours)
+            area_hull_list.append(area_of_hull)
+            area_contour_list.append(area_of_contour)
+            diff.append(area_of_hull-area_of_contour)
+
+        mean_area_aphid = np.mean(area_hull_list)
+
+        OUT_value = []
+        for i in range(len(diff)):
+            if area_hull_list[i] > mean_area_aphid:
+                if diff[i]/area_hull_list[i] > 0.2:
+                    OUT_value.append([i,diff[i]])
+ 
+        M = [np.mean(diff) for _ in range(len(diff))]
+        limit_moyenne_value = np.mean(diff)*3
+
+        OUT_value_y = [OUT_value[i][1] for i in range(len(OUT_value))]
+        OUT_value_x = [OUT_value[i][0] for i in range(len(OUT_value))]
+
+        M = [np.mean(diff) for _ in range(len(diff))]
+        
+        if OUT_value!=[]:
+            dico_out[iw] = OUT_value
             
+    L1 = list(dico_out.keys())
+    names =  []
+    
+    for ix in range(len(L1)):
+        one_image = np.squeeze(imread(L1[ix])[:,:,0])
+        data = np.array(one_image)
+
+        distance = ndi.distance_transform_edt(one_image)
+        coords = peak_local_max(distance, footprint=np.ones((3, 3)), labels=one_image)
+        mask = np.zeros(distance.shape, dtype=bool)
+        mask[tuple(coords.T)] = True
+        markers, _ = ndi.label(mask)
+        labels = watershed(-distance, markers, mask=one_image)
+        labels_data = define_marker(labels)
+        
+        data = np.array(labels_data)
+        fond_image=np.where(data==0)
+        aphid=np.where(data!=0)
+        data[fond_image]=0
+        data[aphid]=255
+        names.append(L1[ix].split('\\')[-2])
+        skimage.io.imsave(L1[ix], data)
+        #plt.imsave(L1[ix], data, cmap = plt.cm.gray)
+
     def open_name(item):
         
         name = item.text()
         name_folder = name[:-3]
+
         
         print('Loading', name, '...')
 
@@ -284,14 +433,7 @@ def process_function_segmentation(napari_viewer : Viewer,filename=pathlib.Path.c
         for fname_i in os.listdir(fname):
             if fname_i.find('result')!=-1:
                 data_label = imread(f'{fname}\{fname_i}')
-                data_label1 = np.array(data_label)
-                fond=np.where(data_label1==85)
-                edge=np.where(data_label1==170)
-                aphid=np.where(data_label1==255)
-                data_label1[fond]=0
-                #data_label1[edge]=170
-                data_label1[edge]=255
-                data_label1[aphid]=255                
+                data_label1 = np.array(data_label)       
                 napari_viewer.add_labels(data_label1,name=f'{fname_i[:-3]}')
             else:
                 napari_viewer.add_image(imread(f'{fname}\{fname_i}'),name=f'{fname_i[:-3]}')
