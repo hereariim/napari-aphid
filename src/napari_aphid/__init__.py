5b3ba8bfae675ae20a73295709d961f7ea014fb6
hereariim <herearii.metuarea@gmail.com>
2023-01-13 11:36:24 +0100

    deelte reader and _sample_data

--- src/napari_aphid/__init__.py
+++ src/napari_aphid/__init__.py
@@ -1,12 +1,8 @@
 __version__ = "0.0.1"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
 from ._widget import process_function_segmentation, save_modification, process_function_classification
 
 __all__ = (
-    "napari_get_reader",
-    "make_sample_data",
     "process_function_segmentation",
     "save_modification",
     "process_function_classification",
