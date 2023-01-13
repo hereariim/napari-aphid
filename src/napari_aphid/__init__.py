95a809c753bf2cd149d0624e7fd67970a9d123c4
hereariim <herearii.metuarea@gmail.com>
2023-01-13 10:35:18 +0100

    delete _writer

--- src/napari_aphid/__init__.py
+++ src/napari_aphid/__init__.py
@@ -3,12 +3,9 @@ __version__ = "0.0.1"
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
 from ._widget import process_function_segmentation, save_modification, process_function_classification
-from ._writer import write_multiple, write_single_image
 
 __all__ = (
     "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
     "make_sample_data",
     "process_function_segmentation",
     "save_modification",
